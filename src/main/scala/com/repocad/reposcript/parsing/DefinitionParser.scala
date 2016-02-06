c8120d353509d4b5368635b21d4fad21959cb7b1
Jegp <jensep@gmail.com>
2016-02-06 18:24:49 +0100

    Refactored function parsing

--- src/main/scala/com/repocad/reposcript/parsing/DefinitionParser.scala
+++ src/main/scala/com/repocad/reposcript/parsing/DefinitionParser.scala
@@ -8,22 +8,9 @@ import com.repocad.reposcript.lexing._
 trait DefinitionParser extends TypedParser with ParserInterface with BlockParser {
 
   def parseDefinition(outerState : ParserState, success : SuccessCont, failure : FailureCont) : Value = {
-    def parseFunctionParameters(parameterTokens : LiveStream[Token],
-                                success : (Seq[RefExpr], LiveStream[Token]) => Value, failure : FailureCont) : Value = {
-      parseUntil(parseParameters, _.head.tag.toString == ")", ParserState(UnitExpr, outerState.env, parameterTokens),
-        parametersState => { parametersState.expr match {
-          case BlockExpr(exprs) if exprs.contains((e : Expr) => !e.isInstanceOf[RefExpr]) =>
-            failure(Error.EXPECTED_PARAMETERS(parameterTokens.toString)(parametersState.position))
-          case BlockExpr(exprs) =>
-            success(exprs.asInstanceOf[Seq[RefExpr]], parametersState.tokens)
-          case unknown => failure(Error.EXPECTED_PARAMETERS(unknown.toString)(parametersState.position))
-        }
-        }, failure)
-    }
-
     def parseFunctionParametersAndBody(innerState : ParserState, success : (Seq[RefExpr], Expr,
       LiveStream[Token]) => Value, failure : FailureCont) : Value = {
-      parseFunctionParameters(innerState.tokens, (parameters, parameterTokens) => {
+      parseFunctionParameters(innerState.tokens, outerState, (parameters, parameterTokens) => {
         parameterTokens match {
           case SymbolToken("=") :~: functionTail =>
             parseFunctionBody(innerState.copy(env = innerState.env, tokens = functionTail), parameters,
@@ -42,7 +29,7 @@ trait DefinitionParser extends TypedParser with ParserInterface with BlockParser
     outerState.tokens match {
       /* Functions or objects */
       /* Prepended function */
-      case PunctToken("(") :~: tail => parseFunctionParameters(tail, (firstParams, firstTail) => {
+      case PunctToken("(") :~: tail => parseFunctionParameters(tail, outerState, (firstParams, firstTail) => {
         firstTail match {
           case SymbolToken(name) :~: PunctToken("(") :~: functionTail =>
             parseFunctionParametersAndBody(outerState.copy(tokens = functionTail), (secondParams, body, bodyTail) => {
@@ -61,7 +48,7 @@ trait DefinitionParser extends TypedParser with ParserInterface with BlockParser
 
       /* Function and object definition */
       case SymbolToken(name) :~: PunctToken("(") :~: tail =>
-        parseFunctionParameters(tail, (parameters, paramsTail) => {
+        parseFunctionParameters(tail, outerState, (parameters, paramsTail) => {
           paramsTail match {
             case SymbolToken("=") :~: bodyTokens =>
               parseFunctionBody(outerState.copy(tokens = bodyTokens), parameters, bodyState => {
@@ -103,6 +90,19 @@ trait DefinitionParser extends TypedParser with ParserInterface with BlockParser
       //success(DefExpr(name, e), env + (name -> e), stream), failure)
 
     }
+  }
+
+  def parseFunctionParameters(parameterTokens : LiveStream[Token], outerState : ParserState,
+                              success : (Seq[RefExpr], LiveStream[Token]) => Value, failure : FailureCont) : Value = {
+    parseUntil(parseParameters, _.head.tag.toString == ")", ParserState(UnitExpr, outerState.env, parameterTokens),
+      parametersState => { parametersState.expr match {
+        case BlockExpr(exprs) if exprs.contains((e : Expr) => !e.isInstanceOf[RefExpr]) =>
+          failure(Error.EXPECTED_PARAMETERS(parameterTokens.toString)(parametersState.position))
+        case BlockExpr(exprs) =>
+          success(exprs.asInstanceOf[Seq[RefExpr]], parametersState.tokens)
+        case unknown => failure(Error.EXPECTED_PARAMETERS(unknown.toString)(parametersState.position))
+      }
+      }, failure)
   }
 
   private def parseParameters(state : ParserState, success : SuccessCont, failure : FailureCont) : Value = {
