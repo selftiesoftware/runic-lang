62e95a349968ac778afdb89d92fb46b8911fac20
Jegp <jensegholm@protonmail.com>
2016-07-14 16:33:01 +0200

    Re-added Main with I/O

---
+++ src/test/scala/com/repocad/reposcript/Main.scala
@@ -0,0 +1,26 @@
+package com.repocad.reposcript
+
+import java.io._
+
+import com.repocad.reposcript.parsing._
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.{BeforeAndAfter, FlatSpec, Matchers}
+
+class MainTest extends FlatSpec with Matchers with MockFactory with BeforeAndAfter {
+
+  "Reposcript command line" should "compile to AST" in {
+    val out = new ByteArrayOutputStream()
+    val in = new ByteArrayInputStream("10".getBytes("UTF8"))
+    scala.Console.withIn(in) {
+      scala.Console.withOut(out) {
+        Main.main(Array("compile"))
+        in.close()
+        out.close()
+        val outString = out.toString("UTF8")
+        //    println(outString)
+        outString should equal(NumberExpr(10).toString)
+      }
+    }
+  }
+
+}
