9d8fcbaf55864ac54220ffc7a2cf07492bf5dd24
Bladtman <sigurt.bladt@gmail.com>
2017-02-11 00:17:53 +0100

    Svg exporter now produces ?xml and svg tag that firefox and chrome will accept

--- src/main/scala/com/repocad/reposcript/export/SvgExporter.scala
+++ src/main/scala/com/repocad/reposcript/export/SvgExporter.scala
@@ -14,21 +14,22 @@ object SvgExporter extends Exporter[String] {
     *
     * A model like {{{ LineModel(0, 0, 10, 5) }}} will result in the following SVG string:
     * {{{
-    *   <svg width="10mm" height="5mm" viewBox="0 0 10 5" fill="none">M0 0 L10 5 Z</svg>
+    *   <?xml version=\"1.0\" encoding=\"utf-8\"?>
+    *   <svg xmlns="http://www.w3.org/2000/svg" width="10mm" height="5mm" viewBox="0 0 10 5" fill="none">M0 0 L10 5 Z</svg>
     * }}}
     *
     * @param model The model to export.
     * @return A String containing a full SVG element, including .
     */
   override def apply(model: ShapeModel): String = {
-    val builder = new StringBuilder()
+    val builder = new StringBuilder() append "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n"
     val viewBox = s"${model.boundary.xMin} ${model.boundary.yMin} ${model.boundary.width} ${model.boundary.height}"
-    builder.append(s"""<svg width="${model.boundary.width}mm" height="${model.boundary.height}mm" viewBox="$viewBox">""")
+    builder.append(s"""<svg xmlns="http://www.w3.org/2000/svg" width="${model.boundary.width}mm" height="${model.boundary.height}mm" viewBox="$viewBox">""")
 
     // Evaluate each model shape and put it in the string builder
-    evaluate(model).foreach(builder.append)
+    evaluate(model).foreach(s => builder.append ("\n" ++ s))
 
-    builder.append("</svg>")
+    builder.append("\n</svg>")
     builder.mkString
   }
 
@@ -38,6 +39,7 @@ object SvgExporter extends Exporter[String] {
 
       case a: ArcModel => Seq(toPath(arcToSvg(a), a.attributes))
       case LineModel(x1, y1, x2, y2, attributes) => Seq(toPath(s"M$x1 $y1 L$x2 $y2 Z", attributes))
+      case bezier: BezierCurveModel => Seq(toPath(bezierCurveModelToSvg(bezier), bezier.attributes))
     }
   }
 
@@ -48,6 +50,10 @@ object SvgExporter extends Exporter[String] {
     var largeArcFlag = if (arc.endAngle - arc.startAngle <= 180) "0" else "1"
 
     s"M${start.x} ${start.y} A${arc.radius} ${arc.radius} 0 $largeArcFlag 0 ${end.x} ${end.y}"
+  }
+
+  def bezierCurveModelToSvg(bezier: BezierCurveModel) : String = {
+    s"M${bezier.x1} ${bezier.y1} C ${bezier.x2} ${bezier.y2} ${bezier.x3} ${bezier.y3} ${bezier.x4} ${bezier.y4}"
   }
 
   private def polarToCartesian(centerX: Double, centerY: Double, radius: Double, angleInDegrees: Double): Vector2D = {
